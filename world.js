c940a9c0def58dc797af0fb22ef35fbcdc48a795
jahneafrancis <jahneafrancis29@gmail.com>
2023-11-20 23:34:26 -0500

    World 2.1

--- world.js
+++ world.js
@@ -1,12 +1,10 @@
 window.onload = function() {
-    var searchButton = document.querySelector('#lookup');
+    var countryLookupButton = document.querySelector('#lookup');
+    var cityLookupButton = document.querySelector('#lookupCities'); // New button for city lookup
 
-    searchButton.onclick = function() {
+    function fetchData(url) {
         var xhr = new XMLHttpRequest();
-        var countryField = document.querySelector('#country');
-        var queryParam = countryField ? encodeURIComponent(countryField.value) : '';
-
-        xhr.open('GET', 'http://localhost/info2180-lab5/world.php?country=' + queryParam, true);
+        xhr.open('GET', url, true);
         xhr.onload = function() {
             if (xhr.status === 200) {
                 document.querySelector('#result').innerHTML = xhr.responseText;
@@ -18,5 +16,17 @@ window.onload = function() {
             document.querySelector('#result').textContent = 'Request failed.';
         };
         xhr.send();
+    }
+
+    countryLookupButton.onclick = function() {
+        var countryField = document.querySelector('#country');
+        var queryParam = countryField ? encodeURIComponent(countryField.value) : '';
+        fetchData('http://localhost/info2180-lab5/world.php?country=' + queryParam);
+    };
+
+    cityLookupButton.onclick = function() {
+        var countryField = document.querySelector('#country');
+        var queryParam = countryField ? encodeURIComponent(countryField.value) : '';
+        fetchData('http://localhost/info2180-lab5/world.php?country=' + queryParam + '&lookup=cities');
     };
 };
--- world.php
+++ world.php
@@ -5,62 +5,87 @@ $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-//$stmt = $conn->query("SELECT * FROM countries");
 
-//$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$action = 'default';
+$results = []; // Initialize $results to avoid undefined variable issues
+
 if (isset($_GET['country'])) {
-    $action = 'search_country';
-}
+    $country = $_GET['country'];
 
-switch ($action) {
-    case 'search_country':
-        $country = $_GET['country'];
-        // Prepare the SQL statement with a placeholder for the country name
-        $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
-        // Use '%' wildcards for partial matching, and bind the country name to the placeholder
+    if (isset($_GET['lookup']) && $_GET['lookup'] === 'cities') {
+        // SQL to fetch cities in the country
+        $stmt = $conn->prepare("SELECT cities.name, cities.district, cities.population
+                                FROM cities
+                                JOIN countries ON cities.country_code = countries.code
+                                WHERE countries.name LIKE :country");
         $country = "%$country%";
         $stmt->bindParam(':country', $country, PDO::PARAM_STR);
-        // Execute the prepared statement
         $stmt->execute();
-        // Fetch the results
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        break;
 
-    default:
-        // If 'country' GET variable is not set, select all countries
-        $stmt = $conn->query("SELECT * FROM countries");
+        // Function to generate table row for cities
+        function generateCityTableRow($row) {
+            return '<tr>'
+                 . '<td>' . htmlspecialchars($row['name'], ENT_QUOTES, 'UTF-8') . '</td>'
+                 . '<td>' . htmlspecialchars($row['district'], ENT_QUOTES, 'UTF-8') . '</td>'
+                 . '<td>' . htmlspecialchars($row['population'], ENT_QUOTES, 'UTF-8') . '</td>'
+                 . '</tr>';
+        }
+
+        $tableRows = array_map('generateCityTableRow', $results);
+        $table = '<table>'
+               . '<tr><th>Name</th><th>District</th><th>Population</th></tr>'
+               . implode("\n", $tableRows)
+               . '</table>';
+
+        echo $table;
+
+    } else {
+        // SQL to fetch country information
+        $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+        $country = "%$country%";
+        $stmt->bindParam(':country', $country, PDO::PARAM_STR);
+        $stmt->execute();
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        break;
+
+        // Function to generate table row for countries
+        function generateCountryTableRow($row) {
+            return '<tr>'
+                 . '<td>' . htmlspecialchars($row['name'], ENT_QUOTES, 'UTF-8') . '</td>'
+                 . '<td>' . htmlspecialchars($row['continent'], ENT_QUOTES, 'UTF-8') . '</td>'
+                 . '<td>' . htmlspecialchars($row['independence_year'], ENT_QUOTES, 'UTF-8') . '</td>'
+                 . '<td>' . htmlspecialchars($row['head_of_state'], ENT_QUOTES, 'UTF-8') . '</td>'
+                 . '</tr>';
+        }
+
+        $tableRows = array_map('generateCountryTableRow', $results);
+        $table = '<table>'
+               . '<tr>'
+               . '<th>Country Name</th>'
+               . '<th>Continent</th>'
+               . '<th>Independence Year</th>'
+               . '<th>Head of State</th>'
+               . '</tr>'
+               . implode("\n", $tableRows)
+               . '</table>';
+
+        echo $table;
+    }
+} else {
+    // Default action: Fetch all countries
+    $stmt = $conn->query("SELECT * FROM countries");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $tableRows = array_map('generateCountryTableRow', $results);
+    $table = '<table>'
+           . '<tr>'
+           . '<th>Country Name</th>'
+           . '<th>Continent</th>'
+           . '<th>Independence Year</th>'
+           . '<th>Head of State</th>'
+           . '</tr>'
+           . implode("\n", $tableRows)
+           . '</table>';
+
+    echo $table;
 }
-function generateTableRow($row) {
-  return '<tr>'
-       . '<td>' . htmlspecialchars($row['name'], ENT_QUOTES, 'UTF-8') . '</td>'
-       . '<td>' . htmlspecialchars($row['continent'], ENT_QUOTES, 'UTF-8') . '</td>'
-       . '<td>' . htmlspecialchars($row['independence_year'], ENT_QUOTES, 'UTF-8') . '</td>'
-       . '<td>' . htmlspecialchars($row['head_of_state'], ENT_QUOTES, 'UTF-8') . '</td>'
-       . '</tr>';
-}
-
-// Assuming $results is already populated with the query results
-$tableRows = array_map('generateTableRow', $results);
-
-// Construct the full table
-$table = '<table>'
-     . '<tr>'
-     . '<th>Country Name</th>'
-     . '<th>Continent</th>'
-     . '<th>Independence Year</th>'
-     . '<th>Head of State</th>'
-     . '</tr>'
-     . implode("\n", $tableRows)
-     . '</table>';
-
-echo $table;
-
 ?>
-<!-- <ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul> -->
